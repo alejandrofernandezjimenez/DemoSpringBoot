324fc6e877def297d250a8da6488346c1acabeac
alejandrofernandezjimenez <alejandro.fernandezjimenez@plexus.es>
2025-03-11 11:39:38 +0100

    Third Commit

--- demo/src/main/java/com/example/demo/ServletInitializer.java
+++ demo/src/main/java/com/example/demo/ServletInitializer.java
@@ -7,7 +7,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		System.out.println("Hola mundo");
 		return application.sources(DemoApplication.class);
 	}
 
